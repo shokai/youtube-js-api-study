50412115c6c66893cbdeefebc82449e0eee61339
Sho Hashimoto <hashimoto@shokai.org>
2013-02-13 04:14:08 +0900

    show video url

--- main.js
+++ main.js
@@ -14,8 +14,17 @@ var onYouTubeStateChange = function(status){
 $(function(){
   var params = { allowScriptAccess: "always" };
   var atts = { id: "player" };
-  var url = "http://www.youtube.com/v/4uq4T22pwaI?enablejsapi=1&playerapiid=youtube_player_api";
+  var url = "http://www.youtube.com/apiplayer?enablejsapi=1&version=3";
   swfobject.embedSWF(url, "youtube_player", "500", "400", "8", null, null, params, atts);
+
+  $("#btn_load").click(function(e){
+    var id = $("#video_id").val();
+    youtube.cueVideoById(id);
+    var url = youtube.getVideoUrl();
+    $("#video_url").html(
+      $("<a>").attr("href", url).text(url)
+    );
+  });
 
   var btn_play = $("#btn_play");
   btn_play.click(function(e){
